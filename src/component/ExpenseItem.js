be63c0009ba006313124a72ae6c456bcae5a7ff9
Sumanth3028 <sumanthakula05@gmail.com>
2023-02-03 02:41:59 -0800

    updated expense

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,24 +1,19 @@
 import Expensecss from "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate'
+import ExpenseDetails from './ExpenseDetails'
+import ExpenseLocation from "./ExpenseLocation";
 function ExpenseItem(props) {
 //   const expenseDate = new Date(2023, 1, 3);
 //   const expenseTitle1 = "Car Insurace";
 //   const expensePrice1 = 200;
-  const expenseTitle2 = "Location";
+ 
 //   const expenseLocation='Hyderabad'
-
+ 
   return (
     <div className="expense-item">
-      <div className="expense-item__description">
-        {props.date.toISOString()}
-      </div>
-      <div className="expense-item__description">
-        <h2> {props.title}</h2>
-        <div className="expense-item__price"> Rs {props.amount}</div>
-      </div>
-      <div className="expense-item__description">
-       <h2>{expenseTitle2}:</h2>
-       <div className="expense-item__description"><h2> {props.location}</h2></div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails title={props.title} amount={props.amount} />
+      <ExpenseLocation location={props.location}></ExpenseLocation>
     </div>
   );
 }
