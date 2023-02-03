b2cb6b1bb28872ced24f12e9c03b300bde3a8275
Sumanth3028 <sumanthakula05@gmail.com>
2023-02-02 22:01:52 -0800

    expense item added

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <div>
+        Food:Rs 300
+      </div>
+
+      <div>
+        Petrol:Rs 100
+      </div>
+
+      <div>
+        Movies:Rs 500
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
