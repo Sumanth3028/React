e0dbdbfbbb1a74d56ff173d6f2591ea7ea43085c
Sumanth3028 <sumanthakula05@gmail.com>
2023-02-04 04:03:42 -0800

    delete added

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Expensecss from "./ExpenseItem.css";
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
@@ -6,20 +6,38 @@ import ExpenseDetails from './ExpenseDetails'
  import ExpenseLocation from "./ExpenseLocation";
 
 const ExpenseItem =(props) => {
-//   const expenseDate = new Date(2023, 1, 3);
-//   const expenseTitle1 = "Car Insurace";
-//   const expensePrice1 = 200;
+
  
 //   const expenseLocation='Hyderabad'
- 
+     
+      
+  const [old,setTitle]=useState(props);
+  // let id=old.title;
+  
+   const removeHandler=()=>
+{
+  //  const removed=old.filter((exp)=>exp.id!==id)
+    
+  const element = document.getElementById("delete");
+  //console.log(element)
+  element.remove()
+      
+      
+      
+    
+}
+  
+
   return (
-    <Card className='expense-item'>
+    <div id='delete'>
+    <Card className='expense-item' >
     
       <ExpenseDate date={props.date} />
       <ExpenseDetails title={props.title} amount={props.amount} />
        <ExpenseLocation location={props.location}></ExpenseLocation> 
-    
+       <button onClick={removeHandler}>Delete Expense</button>
     </Card>
+    </div>
   );
 }
 
