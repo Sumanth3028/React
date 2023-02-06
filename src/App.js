8e35f99a2d87b5eb7af80f441ba7aa91f0a2811d
Sumanth3028 <sumanthakula05@gmail.com>
2023-02-05 20:54:00 -0800

    Added newExpesne,newform and changes in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import ExpenseItem from "./component/ExpenseItem";
-export default function App() {
+import React from 'react'
+import ExpenseItem from "./component/Expenses/ExpenseItem";
+import Expenses from './component/Expenses/Expenses';
+import NewExpense from './component/NewExpense/NewExpense'
+
+ const App =() =>{
   const expenses = [
     {
       id: "e1",
@@ -8,8 +12,8 @@ export default function App() {
       date: new Date(2020, 7, 14),
       location:'hyderabad'
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12),location:'Bangalore' },
-    {
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12),location:'Bangalore' }
+   ,    {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
@@ -21,29 +25,34 @@ export default function App() {
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      location:"chennai"
+       location:"chennai"
     },
   ];
- 
+  
  
   return (
     
     <div>
+      <NewExpense />
+      <Expenses data={expenses} />
       
-      { expenses.map((data) =>(
-         <ExpenseItem  
-          title={data.title}
-          amount={data.amount}
-          date={data.date}
-          location={data.location}
-     
-       ></ExpenseItem> 
-     
-       ))}
     </div>
-    
+  //   <div>
+      
+  //   { expenses.map((data) =>(
+  //      <ExpenseItem  
+  //       title={data.title}
+  //       amount={data.amount}
+  //       date={data.date}
+  //       location={data.location}
+   
+  //    ></ExpenseItem> 
+   
+  //    ))}
+  // </div>
   
   );
   }
+  export default App
 
 
