d2a28f871282ccf732df3f02092269beebbdacd8
Sumanth3028 <sumanthakula05@gmail.com>
2023-02-06 00:42:15 -0800

    Adding dynamic data

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React ,{useState} from "react";
 import './ExpenseForm.css'
-const ExpenseForm=()=>{
+const ExpenseForm=(props)=>{
     const [enteredTitle,setChangedTitle]=useState('')
     const [enteredAmount,setChangedAmount]=useState('')
     const [enteredDate,setChangedDate]=useState('')
@@ -39,21 +39,24 @@ const ExpenseForm=()=>{
             amount:enteredAmount,
             date:new Date(enteredDate)
         }
-        console.log(expense)
+         props.onSaveExpenseData(expense)
+        setChangedAmount('')
+        setChangedTitle('')
+        setChangedDate('')
     }
 return(
       <form onSubmit={formHandler}>
     <div className="new-expense__controls">
     <div className="new-expense__control">
         <label>ExpenseTitle:</label>
-        <input type='text' onChange={titleChangeHandler} />
+        <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         <div className="new-expense__control">
         <label>ExpenseAmount:</label>
-        <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+        <input type='number' min='0.01' step='0.01'  value={enteredAmount} onChange={amountChangeHandler} />
         </div>
     <div className="new-expense__control">
         <label>ExpenseDate:</label>
-        <input type='Date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+        <input type='Date' min='2019-01-01' max='2022-12-31'  value={enteredDate} onChange={dateChangeHandler} />
     </div>   
     </div>
     </div>
